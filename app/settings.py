d445d7082f7b7c1e4979acbf7ab5250089efbe61
BlaineLambert <flambert24@basecampcodingacademy.org>
2024-03-28 14:36:13 -0500

    hooked to postgres database and migrated

--- app/settings.py
+++ app/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'afterglo',  # Replace with your actual database name
+        'USER': 'afterglo',      # Replace with your PostgreSQL username
+        'PASSWORD': 'afterglo',  # Replace with your PostgreSQL password
+        'HOST': 'localhost',     # Replace with your PostgreSQL host
+        'PORT': '',              # Leave empty for default port
     }
 }
 
