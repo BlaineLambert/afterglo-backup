6a3554d91dfd9dc9e54d0ea14a66576867053ccf
BlaineLambert <flambert24@basecampcodingacademy.org>
2024-04-01 11:43:04 -0500

    db configuration

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.TextField()
+    price = models.IntegerField()
+    desc = models.TextField()
+    photo = models.ImageField(upload_to="app/uploads/", null=True, blank=True, default='app/uploads/default.png')
+
+class User(models.Model):
+    name = models.TextField()
+    email = models.EmailField()
+    number = models.TextField()
