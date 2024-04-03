12e44e14d94d61e82495fe7d53628a3a8b2be5a8
BlaineLambert <flambert24@basecampcodingacademy.org>
2024-04-03 14:02:54 -0500

    new logo and working adding to cart feature

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import *
+from app.models import *
 from .forms import *
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http.request import HttpRequest
