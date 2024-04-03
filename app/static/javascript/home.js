ccfbb1ce0f3a8bf79889da80438e91ab06865caf
AJ Gilliland <manuelgillilandiv@gmail.com>
2024-04-03 09:15:19 -0500

    finished skin tone accordian in faq

--- app/static/javascript/home.js
+++ app/static/javascript/home.js
@@ -6,3 +6,23 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
   
+document.addEventListener('DOMContentLoaded', () => {
+  const accordionButtons = document.querySelectorAll('.accordion-button');
+    accordionButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const currentlyActiveAccordionItemContent = document.querySelector('.accordion-content.max-h-xl');
+            const newAccordionContent = button.nextElementSibling;
+            if (currentlyActiveAccordionItemContent && currentlyActiveAccordionItemContent !== newAccordionContent) {
+                currentlyActiveAccordionItemContent.classList.remove('max-h-xl');
+                currentlyActiveAccordionItemContent.style.maxHeight = '0';
+            }
+            if (newAccordionContent.style.maxHeight === '0px' || !newAccordionContent.style.maxHeight) {
+                newAccordionContent.classList.add('max-h-xl');
+                newAccordionContent.style.maxHeight = newAccordionContent.scrollHeight + 'px';
+            } else {
+                newAccordionContent.classList.remove('max-h-xl');
+                newAccordionContent.style.maxHeight = '0';
+            }
+        });
+    });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
   './app/**/*.js',],
   theme: {
     extend: {
-      colors: {gold: '#ffd700',},
+      colors: {
+        gold: '#ffd700',
+        skin: {
+          lightest: '#ffdbac',
+          light: '#f1c27d',
+          medium: '#e0ac69',
+          dark: '#c68642',
+          darkest: '#8d5524',
+        },
+      },
     },
   },
   plugins: [],
