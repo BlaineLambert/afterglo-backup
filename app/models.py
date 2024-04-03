ece69ae1e19c12d72ae1dacb97240ce7c40f39fd
BlaineLambert <flambert24@basecampcodingacademy.org>
2024-04-02 19:09:46 -0500

    more product page styling

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Product(models.Model):
-    name = models.CharField(max_length = 24)
+    name = models.CharField(max_length = 40)
     price = models.IntegerField()
     desc = models.CharField(max_length = 255)
     photo = models.ImageField(upload_to="app/uploads/", null=True, blank=True, default='app/uploads/default.png')
